6bd563975eb3cfd7054e251c3ceb057f95983d92
“ravinrawat” <ravinrawat.wd@gmail.com>
2024-01-23 18:47:04 -0700

    Added test grid

--- lib/categories.dart
+++ lib/categories.dart
@@ -9,8 +9,15 @@ class CategoriesScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Categories'),
       ),
-      body: const Center(
-        child: Text('Categories'),
+      body: Center(
+        child: GridView(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3,
+              childAspectRatio: 3 / 2,
+              mainAxisSpacing: 20,
+              crossAxisSpacing: 20,
+            ),
+            children: const [Text("item 1"), Text("item 2"), Text("item 3")]),
       ),
     );
   }
