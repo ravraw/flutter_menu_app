a28c70f37495653895135521b5c5623aff04cc9d
“ravinrawat” <ravinrawat.wd@gmail.com>
2024-01-26 19:46:18 -0700

    Started TabsScreen work

---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class TabsScreen extends StatefulWidget {
+  const TabsScreen({super.key});
+
+  @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Dynamic Tabs"),
+        ),
+        body: const Center(
+          child: Text("Tabs"),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: (index) {},
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.category),
+              label: "Categories",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.star),
+              label: "Favorites",
+            ),
+          ],
+        ));
+  }
+}
