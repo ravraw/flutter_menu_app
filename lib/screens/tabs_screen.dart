e03d2c332c8f311c422702c7c3c5d2b24fd5024e
“ravinrawat” <ravinrawat.wd@gmail.com>
2024-01-26 19:57:10 -0700

    Updated TabsScreen

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_menu_app/screens/categories_screen.dart';
+import 'package:flutter_menu_app/screens/meals_screen.dart';
 
 class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
@@ -8,17 +10,33 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  int _selectedPageIndex = 0;
+
+  void _selectpage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget activePage = const CategoriesScreen();
+
+    if (_selectedPageIndex == 1) {
+      activePage = const MealsScreen(
+        "Favorites",
+        [],
+      );
+    }
+
     return Scaffold(
         appBar: AppBar(
           title: const Text("Dynamic Tabs"),
         ),
-        body: const Center(
-          child: Text("Tabs"),
-        ),
+        body: activePage,
         bottomNavigationBar: BottomNavigationBar(
-          onTap: (index) {},
+          onTap: (index) => _selectpage(index),
+          currentIndex: _selectedPageIndex,
           items: const [
             BottomNavigationBarItem(
               icon: Icon(Icons.category),
