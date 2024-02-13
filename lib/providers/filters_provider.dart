275bb56067a114a91a9e8daa8d405e297bfe3c23
“ravinrawat” <ravinrawat.wd@gmail.com>
2024-02-12 20:26:35 -0700

    Added filtersProvider

---
+++ lib/providers/filters_provider.dart
@@ -0,0 +1,27 @@
+import "package:flutter_riverpod/flutter_riverpod.dart";
+
+enum Filter {
+  glutenFree,
+  lactoseFree,
+  vegetarian,
+  vegan,
+}
+
+class FiltersNotifier extends StateNotifier<Map<Filter, bool>> {
+  FiltersNotifier()
+      : super({
+          Filter.glutenFree: false,
+          Filter.lactoseFree: false,
+          Filter.vegetarian: false,
+          Filter.vegan: false,
+        });
+
+  void setFilter(Filter filter, bool value) {
+    state = {...state, filter: value};
+  }
+}
+
+final filtersProvider =
+    StateNotifierProvider<FiltersNotifier, Map<Filter, bool>>(
+  (ref) => FiltersNotifier(),
+);
